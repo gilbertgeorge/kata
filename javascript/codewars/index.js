70e7f16369f63ea72c4dd5c69d6901c176bfec36
GEORGEGPC\George <george@grgilbert.com>
2023-01-22 08:39:14 -0500

    add js

---
+++ javascript/codewars/index.js
@@ -0,0 +1,12 @@
+const {createPhoneNumber} = require("./training/CreatePhoneNumber");
+
+console.log('Code wars kata for js')
+
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+
+console.log('Done.')
