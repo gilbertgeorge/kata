5d6c81913684d3bc1ee705dc712901729e8028be
gilbertgeorge <george@grgilbert.com>
2023-02-08 15:08:48 -0500

    roman numeral converter

---
+++ c#/codewars/codewars/kyu4/RomanNumerals.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace codewars.kyu4;
+
+public class RomanNumerals
+{
+    private static readonly Dictionary<char, int> FromRomanNumerals = new Dictionary<char, int>
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
+    
+    private static readonly Dictionary<int, string> ToRomanNumerals = new Dictionary<int, string>
+    {
+        { 1000, "M" },
+        { 900, "CM" },
+        { 500, "D" },
+        { 400, "CD" },
+        { 100, "C" },
+        { 90, "XC" },
+        { 50, "L" },
+        { 40, "XL" },
+        { 10, "X" },
+        { 9, "IX" },
+        { 5, "V" },
+        { 4, "IV" },
+        { 1, "I" },
+    };
+
+    public static string ToRoman(int n)
+    {
+        var roman = new StringBuilder();
+
+        foreach (var item in ToRomanNumerals)
+        {
+            while (n >= item.Key)
+            {
+                roman.Append(item.Value);
+                n -= item.Key;
+            }
+        }
+
+        return roman.ToString();
+    }
+
+    public static int FromRoman(string romanNumeral)
+    {
+        var total = 0;
+        char? previousRoman = null;
+
+        foreach (var currentRoman in romanNumeral)
+        {
+            var previous = previousRoman != null ? FromRomanNumerals[previousRoman.Value] : 0;
+            var current = FromRomanNumerals[currentRoman];
+
+            if (previous != 0 && current > previous)
+            {
+                total = total - (2 * previous) + current;
+            }
+            else
+            {
+                total += current;
+            }
+
+            previousRoman = currentRoman;
+        }
+
+        return total;
+    }
+}
---
+++ c#/codewars/codewars_tests/kyu4/RomanNumeralsTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using codewars.kyu4;
+
+namespace codewars_tests.kyu4;
+
+[TestClass]
+public class RomanNumeralsTests
+{
+    [TestMethod]
+    public void ToRomanBase()
+    {
+        //arrange
+        var expectations = new Dictionary<int, string>
+        {
+            { 1, "I" },
+            { 2, "II" },
+            { 3, "III" },
+            { 4, "IV" },
+            { 5, "V" },
+            { 6, "VI" },
+            { 9, "IX" },
+            { 27, "XXVII" },
+            { 48, "XLVIII" },
+            { 59, "LIX" },
+            { 93, "XCIII" },
+            { 141, "CXLI" },
+            { 163, "CLXIII" },
+            { 402, "CDII" },
+            { 575, "DLXXV" },
+            { 911, "CMXI" },
+            { 1024, "MXXIV" },
+            { 3000, "MMM" }
+        };
+
+        foreach (var number in expectations)
+        {
+            Assert.AreEqual(number.Value, RomanNumerals.ToRoman(number.Key), "Number: " + number);
+        }
+    }
+    
+    [TestMethod]
+    public void FromRomanBase()
+    {
+        //arrange
+        var expectations = new Dictionary<string, int>
+        {
+            { "I", 1 },
+            { "II", 2 },
+            { "III", 3 },
+            { "IV", 4 },
+            { "V", 5 },
+            { "VI", 6 },
+            { "IX", 9 },
+            { "XXVII", 27 },
+            { "XLVIII", 48 },
+            { "LIX", 59 },
+            { "XCIII", 93 },
+            { "CXLI", 141 },
+            { "CLXIII", 163 },
+            { "CDII", 402 },
+            { "DLXXV", 575 },
+            { "CMXI", 911 },
+            { "MXXIV", 1024 },
+            { "MMM", 3000 }
+        };
+
+        foreach (var number in expectations)
+        {
+            Assert.AreEqual(number.Value, RomanNumerals.FromRoman(number.Key), "Number: " + number);
+        }
+    }
+}
