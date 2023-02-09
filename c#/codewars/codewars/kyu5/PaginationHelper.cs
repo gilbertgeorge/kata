d8cbcf4d054d6a0e1b80d68f5298ce94fc9c43c8
gilbertgeorge <george@grgilbert.com>
2023-02-09 17:32:31 -0500

    c# page class

---
+++ c#/codewars/codewars/kyu5/PaginationHelper.cs
@@ -0,0 +1,72 @@
+﻿namespace codewars.kyu5;
+
+public class PagnationHelper<T>
+{
+    private readonly IList<T> _collection;
+    private readonly int _itemsPerPage;
+    
+    /// <summary>
+    /// Constructor, takes in a list of items and the number of items that fit within a single page
+    /// </summary>
+    /// <param name="collection">A list of items</param>
+    /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+    public PagnationHelper(IList<T> collection, int itemsPerPage)
+    {
+        _itemsPerPage = itemsPerPage;
+        _collection = collection;
+    }
+
+    /// <summary>
+    /// The number of items within the collection
+    /// </summary>
+    public int ItemCount => _collection.Count;
+
+    /// <summary>
+    /// The number of pages
+    /// </summary>
+    public int PageCount
+    {
+        get
+        {
+            var pageCount = ItemCount / _itemsPerPage;
+            if (ItemCount % _itemsPerPage > 0)
+            {
+                pageCount++;
+            }
+            return pageCount;      
+        }
+    }
+
+    /// <summary>
+    /// Returns the number of items in the page at the given page index 
+    /// </summary>
+    /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+    /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+    public int PageItemCount(int pageIndex)
+    {
+        if (pageIndex < 0 || pageIndex >= PageCount)
+        {
+            return -1;
+        }
+        var itemCount = _itemsPerPage;
+        if (pageIndex == PageCount - 1)
+        {
+            itemCount = ItemCount % _itemsPerPage;
+        }
+        return itemCount;
+    }
+
+    /// <summary>
+    /// Returns the page index of the page containing the item at the given item index.
+    /// </summary>
+    /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+    /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
+    public int PageIndex(int itemIndex)
+    {
+        if (itemIndex < 0 || itemIndex >= ItemCount)
+        {
+            return -1;
+        }
+        return itemIndex / _itemsPerPage;    
+    }
+}
---
+++ c#/codewars/codewars_tests/kyu5/PaginationHelperTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using codewars.kyu5;
+
+namespace codewars_tests.kyu5;
+
+[TestClass]
+public class PaginationHelperTests
+{
+    private readonly IList<int> _collection = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+    private PagnationHelper<int> _helper;
+    private readonly IList<char> _collection2 = new List<char>{'a', 'b', 'c', 'd', 'e', 'f'};
+    private PagnationHelper<char> _helper2;
+    
+    [TestInitialize]
+    public void SetUp()
+    {
+        _helper = new PagnationHelper<int>(_collection, 10);
+        _helper2 = new PagnationHelper<char>(_collection2, 4);
+    }
+    
+    [TestMethod]
+    public void PageItemCountTest()
+    {
+        Assert.AreEqual(-1, _helper.PageItemCount(-1));
+        Assert.AreEqual(10, _helper.PageItemCount(1));
+        Assert.AreEqual(-1, _helper.PageItemCount(3));
+        
+        Assert.AreEqual(4, _helper2.PageItemCount(0));
+        Assert.AreEqual(2, _helper2.PageItemCount(1));
+        Assert.AreEqual(-1, _helper2.PageItemCount(2));
+        
+    }
+    
+    [TestMethod]
+    public void PageIndexTest()
+    {
+        Assert.AreEqual(-1, _helper.PageIndex(-1));
+        Assert.AreEqual(1, _helper.PageIndex(12));
+        Assert.AreEqual(-1, _helper.PageIndex(24));
+        
+        Assert.AreEqual(1, _helper2.PageIndex(5));
+        Assert.AreEqual(0, _helper2.PageIndex(2));
+        Assert.AreEqual(-1, _helper2.PageIndex(20));
+        Assert.AreEqual(-1, _helper2.PageIndex(-10));
+    }
+    
+    [TestMethod]
+    public void ItemCountTest()
+    {
+        Assert.AreEqual(24, _helper.ItemCount);
+        
+        Assert.AreEqual(6, _helper2.ItemCount);
+    }
+    
+    [TestMethod]
+    public void PageCountTest()
+    {
+        Assert.AreEqual(3, _helper.PageCount);
+        
+        Assert.AreEqual(2, _helper2.PageCount);
+    }
+    
+}
