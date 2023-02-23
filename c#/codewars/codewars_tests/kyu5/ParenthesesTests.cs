8da0e3cf91b75cd97f230dc5c6ac6f00f562378c
gilbertgeorge <george@grgilbert.com>
2023-02-23 15:27:17 -0500

    Parentheses validator

---
+++ c#/codewars/codewars_tests/kyu5/ParenthesesTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using codewars.kyu5;
+
+namespace codewars_tests.kyu5;
+
+[TestClass]
+public class ParenthesesTests
+{
+    [TestMethod]
+    public void SampleTest1()
+    {
+        Assert.AreEqual(true, Parentheses.ValidParentheses( "()" ));
+    }
+    
+    [TestMethod]
+    public void SampleTest2()
+    {
+        Assert.AreEqual(false, Parentheses.ValidParentheses( ")((((" ));
+    }
+    
+    [TestMethod]
+    public void SampleTest3()
+    {
+        Assert.AreEqual(false, Parentheses.ValidParentheses( ")(()))" ));
+    }
+    
+    [TestMethod]
+    public void SampleTest4()
+    {
+        Assert.AreEqual(false, Parentheses.ValidParentheses( "(" ));
+    }
+    
+    [TestMethod]
+    public void SampleTest5()
+    {
+        Assert.AreEqual(true, Parentheses.ValidParentheses( "(())((()())())" ));
+    }
+}
