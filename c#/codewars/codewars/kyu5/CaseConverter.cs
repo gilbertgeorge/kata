4dd9a28be2731bff71970337af6d0877fd3f9737
gilbertgeorge <george@grgilbert.com>
2023-03-06 17:32:54 -0500

    case converter

---
+++ c#/codewars/codewars/kyu5/CaseConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace codewars.kyu5;
+
+public static class CaseConverter
+{
+    public static string PascalToSnake(int str)
+    {
+        return str.ToString();
+    }
+
+    public static string PascalToSnake(string str) 
+    {
+        var result = "";
+        foreach (var c in str)
+        {
+            if (char.IsUpper(c))
+            {
+                result += "_" + char.ToLower(c);
+            }
+            else
+            {
+                result += c;
+            }
+        }
+        return result.TrimStart('_');
+    }
+}
---
+++ c#/codewars/codewars_tests/kyu5/CaseConverterTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using codewars.kyu5;
+
+namespace codewars_tests.kyu5;
+
+[TestClass]
+public class CaseConverterTests
+{
+    [TestMethod]
+    public void PascalToSnakeTests()
+    {
+        Assert.AreEqual("test_controller", CaseConverter.PascalToSnake("TestController"));
+        Assert.AreEqual("this_is_beautiful_day", CaseConverter.PascalToSnake("ThisIsBeautifulDay"));
+        Assert.AreEqual("am7_days", CaseConverter.PascalToSnake("Am7Days"));
+        Assert.AreEqual("my3_code_is4_times_better", CaseConverter.PascalToSnake("My3CodeIs4TimesBetter"));
+        Assert.AreEqual("arbitrarily_sending_different_types_to_functions_makes_katas_cool", CaseConverter.PascalToSnake("ArbitrarilySendingDifferentTypesToFunctionsMakesKatasCool"));
+        Assert.AreEqual("1", CaseConverter.PascalToSnake(1), "Numbers should be turned to strings!");
+    }
+}
